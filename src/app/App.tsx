f17d1d6a6459cbcf0f4f80e3c07364400f73b655
LDS196 <lihachevd87@gmail.com>
2023-06-19 11:22:05 +0400

    feat: add media queries

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import "./App.css"
+import "./App.scss"
 import { Route, Routes } from "react-router-dom"
 import Home from "../common/components/Home/Home"
 import PersonalData from "../common/components/PersonalData/PersonalData"
